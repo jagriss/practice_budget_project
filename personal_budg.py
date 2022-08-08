815157247b9a3ca1d84d5f43e47eff8d358cae98
Jace Grissom <jacegrissom43@gmail.com>
2022-08-08 14:54:20 -0400

    added result html template

--- personal_budg.py
+++ personal_budg.py
@@ -9,9 +9,16 @@ def budget_greeting():
    return message
 
 @app.route('/form')
-def budget_input():
-   message = "Hello, Saver!"
+def budget_input_form():
    return render_template("personal_budg_form.html")
+
+@app.route('/results', methods=["POST"])
+def budget_input_form():
+   ann_income = request.form['ann_income']
+   housing_cost = request.form['housing_cost']
+   car_pay = request.form['car_pay']
+   insur_pay = request.form['insur_pay']
+   return render_template("personal_budg_results.html", ann_income = ann_income, housing_cost = housing_cost, car_pay = car_pay, insur_pay = insur_pay)
 
 if __name__ == '__main__':
    app.run()
